71830895c144670dd879da3a86cf2948dff63f5f
Rafael Cronemberger <rafael@berger.com.br>
2023-08-13 02:06:29 -0300

    First commit

---
+++ src/Berger.Extensions.Logging/Enums/LogType.cs
@@ -0,0 +1,13 @@
+﻿namespace Berger.Extensions.Logging
+{
+    public enum LogType
+    {
+        Trace = 0,
+        Debug = 1,
+        Information = 2,
+        Warning = 3,
+        Error = 4,
+        Critical = 5,
+        None = 6
+    }
+}
---
+++ src/Berger.Extensions.Logging/Models/LogDetail.cs
@@ -0,0 +1,46 @@
+﻿namespace Berger.Extensions.Logging
+{
+    public class LogDetail
+    {
+        #region Methods
+        public void SetInnerException(string inner)
+        {
+            this.InnerException = inner;
+        }
+        public void SetMemberMap(string member)
+        {
+            this.MemberMap = member;
+        }
+        public void SetMessage(string message)
+        {
+            this.Message = message;
+        }
+        public void SetSource(string source)
+        {
+            this.Source = source;
+        }
+        public void SetStackTrace(string stack)
+        {
+            this.StackTrace = stack;
+        }
+        public void SetTypeMap(string type)
+        {
+            this.TypeMap = type;
+        }
+        public void SetPath(string path)
+        {
+            this.Path = path;
+        }
+        #endregion
+
+        #region Properties
+        public string InnerException { get; set; } = string.Empty;
+        public string MemberMap { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public string Source { get; set; } = string.Empty;
+        public string StackTrace { get; set; } = string.Empty;
+        public string TypeMap { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
+        #endregion
+    }
+}
